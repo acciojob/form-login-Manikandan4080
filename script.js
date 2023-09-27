edd4b00669ea4277af35560d41a3f70a4409da35
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-09-27 19:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getFormvalue() {
 	let f_name =  document.getElementsByName("fname")[0].value
 	let l_name = document.getElementsByName("lname")[0].value
-	window.alert(f_name+" "+l_name)
+	alert(f_name+" "+l_name)
+	const bt = document.getElementById("btn")
+	bt.className="blue_btn"
 }
